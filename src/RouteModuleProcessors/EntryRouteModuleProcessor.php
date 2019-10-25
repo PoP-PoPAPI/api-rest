a2ee638a6e27ae08cb4ef759ecdcf6424ba01ee0
Leo <losoviz@gmail.com>
2019-10-25 15:58:31 +0800

    Organized code under "Conditional"

--- src/RouteModuleProcessors/EntryRouteModuleProcessor.php
+++ src/RouteModuleProcessors/EntryRouteModuleProcessor.php
@@ -2,7 +2,6 @@
 namespace PoP\RESTAPI\RouteModuleProcessors;
 
 use PoP\ModuleRouting\AbstractEntryRouteModuleProcessor;
-use PoP\ComponentModel\Server\Utils;
 use PoP\ComponentModel\Engine_Vars;
 use PoP\Hooks\Facades\HooksAPIFacade;
 use PoP\API\Facades\FieldQueryConvertorFacade;
@@ -36,20 +35,14 @@ class EntryRouteModuleProcessor extends AbstractEntryRouteModuleProcessor
     {
         $ret = array();
 
-        // API
-        if (!Utils::disableAPI()) {
-
-            $vars = Engine_Vars::getVars();
-
-            // Single endpoint REST API
-            $ret[RouteNatures::HOME][] = [
-                'module' => [\PoP_API_Module_Processor_FieldDataloads::class, \PoP_API_Module_Processor_FieldDataloads::MODULE_DATALOAD_DATAQUERY_ROOT_FIELDS, ['fields' => isset($vars['query']) ? $vars['query'] : self::getRESTFields()]],
-                'conditions' => [
-                    'scheme' => POP_SCHEME_API,
-                    'datastructure' => GD_DATALOAD_DATASTRUCTURE_REST,
-                ],
-            ];
-        }
+        $vars = Engine_Vars::getVars();
+        $ret[RouteNatures::HOME][] = [
+            'module' => [\PoP_API_Module_Processor_FieldDataloads::class, \PoP_API_Module_Processor_FieldDataloads::MODULE_DATALOAD_DATAQUERY_ROOT_FIELDS, ['fields' => isset($vars['query']) ? $vars['query'] : self::getRESTFields()]],
+            'conditions' => [
+                'scheme' => POP_SCHEME_API,
+                'datastructure' => GD_DATALOAD_DATASTRUCTURE_REST,
+            ],
+        ];
 
         return $ret;
     }
