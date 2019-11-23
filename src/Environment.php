5ec44bfa716b017afef0681c1728e92326e31968
Leo <losoviz@gmail.com>
2019-11-23 17:09:15 +0800

    Typed functions

--- src/Environment.php
+++ src/Environment.php
@@ -3,7 +3,7 @@ namespace PoP\RESTAPI;
 
 class Environment
 {
-    public static function disableRESTAPI()
+    public static function disableRESTAPI(): bool
     {
         return isset($_ENV['DISABLE_REST_API']) ? strtolower($_ENV['DISABLE_REST_API']) == "true" : false;
     }
