ca8235e8d26790869866bd3a605b238bd1ad9fa9
Leo <losoviz@gmail.com>
2020-04-13 19:08:01 +0800

    Added declare(strict_types=1);

--- src/Component.php
+++ src/Component.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\RESTAPI;
 
 use PoP\Root\Component\AbstractComponent;
--- src/Config/ServiceConfiguration.php
+++ src/Config/ServiceConfiguration.php
@@ -1,4 +1,7 @@
 <?php
+
+declare(strict_types=1);
+
 namespace PoP\RESTAPI\Config;
 
 use PoP\ComponentModel\Container\ContainerBuilderUtils;
